bb5edb0f0604ef33b9d700fb696ab8f49adba0ec
Phillip Webb <pwebb@vmware.com>
2022-08-29 21:43:45 -0700

    Hacking

---
+++ src/test/java/com/example/aottestdemo/AottestdemoApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.aottestdemo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+class AottestdemoApplicationTests {
+
+	@Autowired
+	private MyBean myBean;
+
+	@Test
+	void contextLoads() {
+		assertThat(this.myBean).isNotNull();
+	}
+
+	@Configuration
+	static class MyConfig {
+
+		@Bean
+		MyBean myBean() {
+			return new MyBean();
+		}
+
+	}
+
+	static class MyBean {
+
+	}
+
+}
